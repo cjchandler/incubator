2fed8d28acd458d58280e2cf06e4b4d746380240
cjchandler <carljosephchandler@gmail.com>
2025-02-10 13:05:12 -0400

    auto update

--- incubator/main_loopV3_incubator.py
+++ incubator/main_loopV3_incubator.py
@@ -99,7 +99,7 @@ def init_state_dict():
     state_dict['target_temperature'] = 37.5
     state_dict['cooling_start_temperature'] = 38
 
-    state_dict['heating_proportional_Cf'] = 0.1
+    state_dict['heating_proportional_Cf'] = 0.02
     state_dict['heating_integral_Cf'] = 0.0001 #2 p , 0.001i was too big perhaps 
     state_dict['heating_derivitive_Cf'] = 0.0
     state_dict['target_humidity'] = 0.37
