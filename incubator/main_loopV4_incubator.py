7640ff8111dd5aa045a4b9db64cb4d0d28d9e36f
cjchandler <carljosephchandler@gmail.com>
2025-04-07 15:58:11 -0300

    auto update

--- incubator/main_loopV4_incubator.py
+++ incubator/main_loopV4_incubator.py
@@ -102,7 +102,7 @@ def init_state_dict():
     state_dict['heating_proportional_Cf'] = 1.90
     state_dict['heating_integral_Cf'] = 0.006 #2 p , 0.001i was too big perhaps 
     state_dict['heating_derivitive_Cf'] = 0.0
-    state_dict['target_humidity'] = 0.37
+    state_dict['target_humidity'] = 0.63
     state_dict['range_humidity'] = 0.03 #can be plus or minus this before we try to fix it  
     state_dict['control_change_minimum_secs'] = 2
     state_dict['last_control_change_timestamp'] = 0
