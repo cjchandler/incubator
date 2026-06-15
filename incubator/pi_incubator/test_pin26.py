6ce339cc7a98cfad334cfdc423783aedcc4bf574
Carl <carljosephchandler@gmail.com>
2026-06-15 14:24:58 -0300

    pi this test of the pin 26 works with the vent motor driver

--- incubator/pi_incubator/test_pin26.py
+++ incubator/pi_incubator/test_pin26.py
@@ -1,9 +1,13 @@
 from gpiozero import LED
 from time import sleep
 
-led = LED(6)
-
+led26 = LED(26)
+led6  = LED(6)
 while True:
-    led.on()
-    sleep(1)
+    led6.off()
+    led26.on()
+    sleep(5)
+    led26.off()
+    led6.on()
+    sleep(5)
     
