a557c092a8f4c7516832276d508c295c9a1c66bf
Carl <carljosephchandler@gmail.com>
2026-06-16 13:41:52 -0300

    test humid

--- incubator/pi_incubator/testpionewire.py
+++ incubator/pi_incubator/testpionewire.py
@@ -1,6 +1,12 @@
 #testing one wire temp sensor 
 import RPi.GPIO
+import time 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.IN , pull_up_down=GPIO.PUD_UP)
 
-time.sleep(100000)
+from w1thermsensor import W1ThermSensor , Unit
+sensor = W1ThermSensor()
+while True:
+	time.sleep(1)
+	t = sensor.get_temperature()
+	print(t)
