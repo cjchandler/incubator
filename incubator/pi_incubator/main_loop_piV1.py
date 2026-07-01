25db4e621743556cdb10b752a99dcaa4ee4b28d7
Carl <carljosephchandler@gmail.com>
2026-07-01 11:07:02 -0300

    main try

--- incubator/pi_incubator/main_loop_piV1.py
+++ incubator/pi_incubator/main_loop_piV1.py
@@ -160,7 +160,7 @@ def init_state_dict():
     state_dict['egg_turning_on'] = True
 
     
-    state_dict['target_temperature'] =17.5
+    state_dict['target_temperature'] =24.5
     state_dict['boost_temperature'] =16.8 #turn on the big heater if it's below boost temperature. Then the little heater is just fine tuning with pid controls
     state_dict['cooling_start_temperature'] = 38.6
 
