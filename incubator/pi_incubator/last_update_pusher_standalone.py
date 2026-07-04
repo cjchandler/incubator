b1d672c6ec9ed358efa582832eab2ff3974e832f
Carl <carljosephchandler@gmail.com>
2026-07-03 21:44:38 -0300

    lastupdate repo code added

--- incubator/pi_incubator/last_update_pusher_standalone.py
+++ incubator/pi_incubator/last_update_pusher_standalone.py
@@ -43,24 +43,25 @@ def push_latest_timestamp_if_needed( timein , path_to_last_update_repo , project
     last_push_timestamp = float(tpush)
     print("last push was at timestamp: ",last_push_timestamp) 
     print("next push is at :", last_push_timestamp + push_interval_sec)
-    print("time until next push : " ,last_push_timestamp + push_interval_sec - tpush )
+    print("time until next push : " ,last_push_timestamp + push_interval_sec -time.time() )
 
     
     #push if needed now
     if( time.time() > last_push_timestamp + push_interval_sec):
+        print("pushing to git")
         push_latest_timestamp(path_to_last_update_repo , project_name_txt)
 
 controls_path = "/home/carl/Git_Projects/incubator/incubator/pi_incubator/datalog/"
 last_update_path = "/home/carl/Git_Projects/last_update_repo/"
 
 while True: 
-	filename = "today_data_piV1.csv"
-	df = pd.read_csv(controls_path + filename)
-	print(df.tail(20))
-	tsaved = df[df.columns[2]].iloc[-1] #2 is the last time saved column
-	
-	push_latest_timestamp_if_needed( tsaved , last_update_path, "pi_V1_incubator_running.txt" , 60*2)
-	time.sleep(30)
+    filename = "today_data_piV1.csv"
+    df = pd.read_csv(controls_path + filename)
+    print(df.tail(20))
+    tsaved = df[df.columns[2]].iloc[-1] #2 is the last time saved column
+    
+    push_latest_timestamp_if_needed( tsaved , last_update_path, "pi_V1_incubator_running.txt" , 60*2)
+    time.sleep(30)
 
-	
+    
 
