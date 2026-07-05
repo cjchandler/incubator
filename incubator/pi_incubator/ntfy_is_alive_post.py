d272c432bef9e993f29e529cf7ab65eb49e8c756
Carl <carljosephchandler@gmail.com>
2026-07-04 21:35:22 -0300

    ntfy thing post test

--- incubator/pi_incubator/ntfy_is_alive_post.py
+++ incubator/pi_incubator/ntfy_is_alive_post.py
@@ -6,14 +6,18 @@ import pandas as pd
    
 #send a message saysing all's well, I'm alive, here's the latest timestamp
 while True: 
-    #look up the file:
-    filepath = "/home/carl/Git_Projects/incubator/incubator/pi_incubator/datalog/today_data_piV1.csv"
-    #look at the pandas thing for last timestamp
-    df = pd.read_csv(filepath)
-    print(df.tail(20))
-    tsaved = df[df.columns[2]].iloc[-1] #2 is the last time saved column
-    
-    #send that data out as a post
-    requests.post("https://ntfy.sh/incubator_test_b5a5n1", data=str(tsaved).encode(encoding='utf-8'))   
+	try: 
+		#look up the file:
+		filepath = "/home/carl/Git_Projects/incubator/incubator/pi_incubator/datalog/today_data_piV1.csv"
+		#look at the pandas thing for last timestamp
+		df = pd.read_csv(filepath)
+		print(df.tail(20))
+		tsaved = df[df.columns[2]].iloc[-1] #2 is the last time saved column
+		
+		#send that data out as a post
+		requests.post("https://ntfy.sh/incubator_piV1_b5a5n1", data=str(tsaved).encode(encoding='utf-8'))   
 
-
+	except:
+		pass 
+		
+	time.sleep(2*60)
