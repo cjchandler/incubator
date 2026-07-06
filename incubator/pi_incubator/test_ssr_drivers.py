ce1d34b2c72e0cdcc471e0c898419316981514c9
Carl <carljosephchandler@gmail.com>
2026-07-06 20:00:08 -0300

    try 12 v heat

--- incubator/pi_incubator/test_ssr_drivers.py
+++ incubator/pi_incubator/test_ssr_drivers.py
@@ -17,7 +17,7 @@ import time
         # ~ ssr_pin.off()
 
 
-ssr_pinHE = LED(21)
+ssr_pinHE = LED(27)
 def heat_12v(inputval): #0 for no heat, 1 for heat 
     
     
@@ -27,8 +27,8 @@ def heat_12v(inputval): #0 for no heat, 1 for heat
         ssr_pinHE.off
 
 while True: 
-	print("heat on 1" )
-	heat_12v(1) 
+	# ~ print("heat on 1" )
+	# ~ heat_12v(1) 
 	time.sleep(5)
 	print( "heat off 0")
 	heat_12v(0) 
