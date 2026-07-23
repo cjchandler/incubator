ca9832acb90dae7c82e5428f8c7cb98c7ae7dec4
Carl <carljosephchandler@gmail.com>
2026-07-23 20:10:31 -0300

    multi sensor i2c test

--- incubator/pi_incubator/test_humid_sensor_i2c.py
+++ incubator/pi_incubator/test_humid_sensor_i2c.py
@@ -11,23 +11,35 @@ import adafruit_sht4x
 from adafruit_extended_bus import ExtendedI2C as I2C
 
 # Create library object using our Extended Bus I2C port
-i2c = I2C(6)  # Device is /dev/i2c-6
+i2c1 = I2C(1)  # Device is /dev/i2c-6
+i2c6 = I2C(6)  # Device is /dev/i2c-6
 
 ###end of i2c6 stuff
 
 
 
-sht = adafruit_sht4x.SHT4x(i2c)
-print("Found SHT4x with serial number", hex(sht.serial_number))
+sht6 = adafruit_sht4x.SHT4x(i2c6)
+sht1 = adafruit_sht4x.SHT4x(i2c1)
+print("Found SHT4x_1 with serial number", hex(sht1.serial_number))
+print("Found SHT4x_6 with serial number", hex(sht6.serial_number))
 
-sht.mode = adafruit_sht4x.Mode.NOHEAT_HIGHPRECISION
+sht1.mode = adafruit_sht4x.Mode.NOHEAT_HIGHPRECISION
+sht6.mode = adafruit_sht4x.Mode.NOHEAT_HIGHPRECISION
 # Can also set the mode to enable heater
 # sht.mode = adafruit_sht4x.Mode.LOWHEAT_100MS
-print("Current mode is: ", adafruit_sht4x.Mode.string[sht.mode])
+print("Current mode 1 is: ", adafruit_sht4x.Mode.string[sht.mode])
+print("Current mode 6 is: ", adafruit_sht4x.Mode.string[sht.mode])
 
 while True:
-    temperature, relative_humidity = sht.measurements
+    temperature, relative_humidity = sht1.measurements
+    temperature6, relative_humidity6 = sht6.measurements
+    print("sensor 1")
     print(f"Temperature: {temperature:0.1f} C")
     print(f"Humidity: {relative_humidity:0.1f} %")
+    print("")
+    print("sensor 6")
+    print(f"Temperature: {temperature6:0.1f} C")
+    print(f"Humidity: {relative_humidity6:0.1f} %")
+    print("")
     print("")
     time.sleep(10)
