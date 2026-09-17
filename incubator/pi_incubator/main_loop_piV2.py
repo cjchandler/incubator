98566b86c345eeab8a5f6c516468823563b76de5
Carl <carljosephchandler@gmail.com>
2026-09-17 19:54:13 -0300

    tests

--- incubator/pi_incubator/main_loop_piV2.py
+++ incubator/pi_incubator/main_loop_piV2.py
@@ -181,8 +181,8 @@ def init_state_dict():
     state_dict['heating_proportional_Cf'] =   .95
     state_dict['heating_integral_Cf'] = 0.05 #2 p , 0.001i was too big perhaps 
     state_dict['heating_derivitive_Cf'] = 0.0
-    state_dict['target_humidity'] = 0.7
-    state_dict['range_humidity'] = 0.03 #can be plus or minus this before we try to fix it  
+    state_dict['target_humidity'] = 0.58
+    state_dict['range_humidity'] = 0.01 #can be plus or minus this before we try to fix it  
     state_dict['control_change_minimum_secs'] = 2
     state_dict['last_control_change_timestamp'] = 0
     
